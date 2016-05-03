9d7f274bc4594d82f00bfec4a354be72fc3169cf
john_a_griffin <john.griffin.simontech@gmail.com>
2016-05-03 22:23:01 +0000

    fwitter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,27 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
+ configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-  end
+    enable :sessions
+    set :session_secret, "fwitter_secret"
+ end
+
+   get '/login' do
+     erb :login
+   end
+
+   post '/login' do
+      user = User.find_by(:username => params[:username])
+      if user
+         session[:user_id] = user.id
+         redirect "/"
+      else
+         redirect "/signup"
+      end
+   end 
+
 
   get '/' do
     @tweets = Tweet.all
@@ -14,24 +31,64 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweet' do
-    erb :tweet
-  end
+    if session[:user_id] #returns nil if there isn't one
+      erb :tweet
+    else
+      redirect "/login"
+    end
+end  
 
   post '/tweet' do
-    user = User.find_by(:username => params[:username])
-    tweet = Tweet.new(:user => user, :status => params[:status])
-    tweet.save
-    redirect '/'
-  end
-
+     user = User.find_by(:id => session[:user_id])
+     tweet = Tweet.new(:user => user, :status => params[:status])
+     tweet.save
+     redirect '/'
+   end
+  
   get '/users' do
     @users = User.all
     erb :users
   end
-
-  post '/sign-up' do 
+ 
+  get '/signup' do
+    @users = User.all
+    erb :users
+  end
+  
+  
+  post '/sign-up' do
     @user = User.new(:username => params[:username], :email => params[:email])
     @user.save
     redirect '/'
   end
+ 
+  get '/logout' do
+     session.destroy
+     redirect '/login'
+  end
+
+
+ configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+ end
+ 
+   helpers do
+     def logged_in?
+       session[:user_id]
+     end
+  
+      def current_user
+        User.find(session[:user_id])
+      end
+   end
+
 end
+
+
+
+
+
+
+
+
